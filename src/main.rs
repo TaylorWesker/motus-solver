49053143021f47cebd84d9d2be04fccc1a53dfe8
Taylor Wesker <marz.arthur@gmail.com>
2022-03-09 17:54:54 +0100

    full entropy calculation implemented

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::fs;
+use itertools::Itertools;
 
 const MAX_CHAR_LEN: usize = 6;
 
@@ -12,17 +13,25 @@ enum Correctness {
 
 struct GuessResult<'a> {
     word: &'a str,
-    correctness: [Correctness; MAX_CHAR_LEN],
+    correctness: Vec<Correctness>,
 }
 
 impl<'a> GuessResult<'a> {
-    fn compute(target: &'_ str, guess: &'a str) -> GuessResult<'a> {
+
+    fn init(w: &'a str, pattern: Vec<Correctness>) -> Self {
+        Self {
+            word: w,
+            correctness: pattern
+        }
+    }
+
+    fn compute(target: &'_ str, guess: &'a str) -> Self {
         let mut ret = GuessResult {
             word: guess,
-            correctness: [Correctness::Absent; MAX_CHAR_LEN],
+            correctness: vec![Correctness::Absent; target.len()],
         };
 
-        let mut used = [false; MAX_CHAR_LEN];
+        let mut used = vec![false; target.len()];
 
         for (i, c) in guess.chars().enumerate() {
             if target.chars().nth(i) == Some(c) {
@@ -53,7 +62,7 @@ impl<'a> GuessResult<'a> {
     }
 }
 
-fn main() {
+fn generate_data(word_size: usize) {
     let filename = "data/pli07.txt";
     let contents = fs::read_to_string(filename)
         .expect("Something went wrong reading the file")
@@ -61,27 +70,40 @@ fn main() {
 
     let words: Vec<&str> = contents
         .split("\n")
-        .filter(|w| w.chars().count() == MAX_CHAR_LEN)
+        .filter(|w| w.chars().count() == word_size)
         .collect();
     
     let total_words = words.len() as f64;
 
-
-    for target in words.iter() {
+    println!("word, entropy");
+    for g in words.iter() {
         let mut entropy = 0.0;
-        // println!("patern matching for '{}'", target);
-        for w in words.iter() {
-            let guess = GuessResult::compute(target, w);
+        // println!("patern matching for '{}'", g);
+        for p in (0..word_size).map(|_| [Correctness::Correct, Correctness::Misplaced, Correctness::Absent]).multi_cartesian_product() {
+            let guess = GuessResult::init(g, p);
             let matching_count = words.iter().filter(|w| guess.r#match(w)).count() as f64;
+            if matching_count == 0.0 {
+                continue;
+            }
+            let prob = matching_count / total_words;
+            entropy += -(prob * prob.log2())
             // println!(
-            //     "    {} {}",
-            //     w,
-            //     words.iter().filter(|w| guess.r#match(w)).count()
+            //     "    {} {:?} {}",
+            //     g,
+            //     p,
+            //     matching_count
             // );
-            let p = matching_count / total_words;
-            entropy += -(p * p.log2());
         }
-        // entropy /= total_words;
-        println!("entropy for '{}': {}", target, entropy);
+        // println!("word entropy for '{}' : {}", g, entropy);
+        println!("{}, {}", g, entropy);
+
     }
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let args = &args[1..];
+    println!("{:?}", args);
+    let char_len = args[0].parse::<usize>().unwrap();
+    generate_data(char_len);
 }
